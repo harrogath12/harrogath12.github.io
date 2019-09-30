fc3646a78d33dc056ff4e85ca066f4894178ea50
harrogath12 <53153529+harrogath12@users.noreply.github.com>
2019-09-30 22:44:17 +0300

    Commit Homework_9_bootstrap_Oleg Bershadsky

---
+++ Homework_9_bootstrap_Oleg Bershadsky/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+
+
+        // плавное перемещение страницы к нужному блоку
+    $("a.go").click(function (e) {
+        e.preventDefault();
+        elementClick = $(this).attr("href");
+        destination = $(elementClick).offset().top;
+        $("body,html").animate({scrollTop: destination }, 800);
+    });
+    });
